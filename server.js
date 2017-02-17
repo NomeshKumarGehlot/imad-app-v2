ab90037f0446a507af31277f5d4ef3160de3378e
NomeshKumarGehlot <gehlot.nomesh@gmail.com>
2017-02-17 05:54:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/airtical-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.send('Airtical One Requested and will be served here');
 });
 
 app.get('/ui/style.css', function (req, res) {
